2d8312fe85c26a7ed6cf2c1b14bcd38b37343ef2
Max Sealey <maxsealey@users.noreply.github.com>
2022-03-06 18:45:16 -0700

    Set up App Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,7 +10,10 @@ class App extends React.Component() {
       <div>
         <h1>Ja<span class="highlight">mmm</span>ing</h1>
         <div className="App">
+          <SearchBar />
           <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
           </div>
         </div>
       </div>
