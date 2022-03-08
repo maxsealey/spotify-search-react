50dd2ab71c792f4d1988c8d892aabdcf638b2cc5
Max Sealey <maxsealey@users.noreply.github.com>
2022-03-07 19:55:58 -0700

    TrackList html and css

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track';
+
+class TrackList extends React.Component {
+    render() {
+        <div className = "TrackList">
+            <Track />
+            <Track />
+            <Track />
+        </div>
+    }
+}
+
+export default TrackList;
